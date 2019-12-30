d2e35ff0819cca204f4298bbf0b536d28cf1e596
taoing <529982750@qq.com>
2019-12-30 23:34:43 +0800

    zuul simple config

--- zuulsvr/src/main/java/com/taoing/zuulsvr/ZuulsvrApplication.java
+++ zuulsvr/src/main/java/com/taoing/zuulsvr/ZuulsvrApplication.java
@@ -1,17 +1,17 @@
-package com.taoing.confsvr;
+package com.taoing.zuulsvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfsvrApplication {
+@EnableZuulProxy
+public class ZuulsvrApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfsvrApplication.class, args);
+        SpringApplication.run(ZuulsvrApplication.class, args);
     }
 
 }
